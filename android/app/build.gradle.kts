cf8f7710f9bf6b200e4908e27044a92a198c0960
Marat <setolavyhamru@gmail.com>
2026-09-09 20:10:08 +0500

    fix(android): upgrade gradle wrapper to 8.14, AGP 8.11.1, kotlin 2.2.20 and fix release config

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,13 +11,14 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility JavaVersion.VERSION_17
-        targetCompatibility JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
     }
+
     kotlinOptions {
-        jvmTarget = '17'
+        jvmTarget = "17"
     }
-}
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
